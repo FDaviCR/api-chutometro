3fc06955f18d15042f6267ee29a7baee8fb92d13
Davi <davicamiloribeiro@gmail.com>
2022-05-25 16:07:14 -0300

    Model de guess

---
+++ src/models/Guess.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database/pg';
+
+export interface GuessInstance extends Model {
+    id: number;
+    name: string;
+    edition: number;
+}
+
+const Guess = sequelize.define<GuessInstance>('Guess', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    },
+    idJogo: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    idVencedor: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    tableName: 'guess',
+    timestamps: false
+});
+
+Guess.sync({force: false});
+module.exports = Guess;
