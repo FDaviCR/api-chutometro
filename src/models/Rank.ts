c79eecfcaf0043be49f3cc7c2e68ceba8468a0bb
Davi <davicamiloribeiro@gmail.com>
2022-05-25 16:20:59 -0300

    Model de rank

---
+++ src/models/Rank.ts
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database/pg';
+
+export interface RankInstance extends Model {
+    id: number;
+    idUser: number;
+    points: number;
+}
+
+const Rank = sequelize.define<RankInstance>('Rank', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    },
+    idUser: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    points:{
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        defaultValue: 0
+    }
+}, {
+    tableName: 'rank',
+    timestamps: false
+});
+
+Rank.sync({force: false});
+module.exports = Rank;
